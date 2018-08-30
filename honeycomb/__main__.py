d20636dbc66c1660061dec9d865d4db735f0c47e
Jonathan Claudius <jclaudius@mozilla.com>
2018-08-30 16:36:26 -0400

    Moneypath logging facility to send to local4.*

--- honeycomb/__main__.py
+++ honeycomb/__main__.py
@@ -12,6 +12,11 @@ import sys
 
 from honeycomb.cli import cli
 
+# Monkey Patching LOG_USER #####
+import logging
+from logging import handlers
+logging.handlers.SysLogHandler.LOG_USER = 20
+### end monkey patch ###
 
 def main():
     """Provide an entry point for setup.py console_scripts."""
